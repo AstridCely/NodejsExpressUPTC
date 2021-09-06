d0eff9be78c163ac83e42c68dd57427ca247b1db
Astrid <61441594+AstridCely@users.noreply.github.com>
2021-09-05 20:11:48 -0500

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,42 @@ const router = express.Router();
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 
+const  credential = {
+  email : "Astrid@gmail.com",
+  password : "Dudu123"
+}
+
+const  credential2 = {
+  email : "Usuario@gmail.com",
+  password : "User1"
+}
+
 const json_Movies = fs.readFileSync('src/Movies.json', 'utf-8');
 let Movies = JSON.parse(json_Movies);
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) =>{
+  res.render('Login', { title : "Inicie Sesion"});
+})
+
+router.post('/login', (req, res)=>{
+  if(req.body.email == credential.email && req.body.password == credential.password){
+      req.session.user = req.body.email;
+      res.redirect('/index');
+  }
+  else if(req.body.email == credential2.email && req.body.password == credential2.password){
+    req.session.user = req.body.email;
+    res.redirect('/indexUser');
+    
+  }else{
+      res.end("Invalid Username")
+  }
+});
+router.get('/index', (req, res) => {
   res.render('index', { Movies});
+});
+
+router.get('/indexUser', (req, res) => {
+  res.render('indexUser', { Movies});
 });
 
 router.get('/newMovie', (req, res) => {
@@ -16,9 +47,9 @@ router.get('/newMovie', (req, res) => {
 
 router.post('/newMovie', (req, res) => {
 
-  const { title, author, image, description } = req.body;
+  const { title, gender, image, Duracion, description } = req.body;
 
-  if (!title || !author || !image || !description) {
+  if (!title || !gender ||  !image  || ! Duracion || !description) {
     res.status(400).send("Entries must have a title and body");
     return;
   }
@@ -26,7 +57,8 @@ router.post('/newMovie', (req, res) => {
   var newMovie = {
     id: uuidv4(),
     title,
-    author,
+    gender,
+    Duracion,
     image,
     description
   };
@@ -38,17 +70,25 @@ router.post('/newMovie', (req, res) => {
   const json_Movies = JSON.stringify(Movies);
   fs.writeFileSync('src/Movies.json', json_Movies, 'utf-8');
 
-  res.redirect('/');
+  res.redirect('/index');
 });
 
-router.get('/delete/:id', (req, res) => {
+router.get('/Buy/:id', (req, res) => {
   Movies = Movies.filter(Movies => Movies.id != req.params.id);
-
   // guardando
   const json_Movies = JSON.stringify(Movies);
   fs.writeFileSync('src/Movies.json', json_Movies, 'utf-8');
 
-  res.redirect('/')
+  res.redirect('/indexUser')
+});
+
+router.get('/delete/:id', (req, res) => {
+  Movies = Movies.filter(Movies => Movies.id != req.params.id);
+  // guardando
+  const json_Movies = JSON.stringify(Movies);
+  fs.writeFileSync('src/Movies.json', json_Movies, 'utf-8');
+
+  res.redirect('/index')
 });
 
 module.exports = router;
